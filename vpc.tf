f77bc1b98c5922dedc7057cbec626dca0de65a49
Chris Bautista <topherianlimited@gmail.com>
2021-05-18 11:29:43 +0000

    Added vpc module 2

--- vpc.tf
+++ vpc.tf
@@ -2,14 +2,14 @@ module "network" {
   source  = "app.terraform.io/topherian-gcp-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "Guarav network"
-  project_id = var.project
+  network_name = "guarav-network"
+  project_id   = var.project
   subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
 
